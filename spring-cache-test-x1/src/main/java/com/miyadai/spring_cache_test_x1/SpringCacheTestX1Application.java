00e7d04e7df7a5a375a1a2f51f7e34fe65035739
kohei-miyadai07 <wm25gb87@gmail.com>
2024-12-22 23:37:54 +0900

    feat: プロジェクト作成

---
+++ spring-cache-test-x1/src/main/java/com/miyadai/spring_cache_test_x1/SpringCacheTestX1Application.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestX1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheTestX1Application.class, args);
+	}
+
+}
