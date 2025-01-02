3e68028e2e00680dbafbbcc3c907951f274fc22a
kohei-miyadai07 <wm25gb87@gmail.com>
2025-01-03 08:38:04 +0900

    feat: CacheManager勉強用のプロジェクトを作成
    refs #feature-001

---
+++ spring-cache-test-x2/src/main/java/com/miyadai/spring_cache_test_x2/SpringCacheTestX2Application.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestX2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheTestX2Application.class, args);
+	}
+
+}
