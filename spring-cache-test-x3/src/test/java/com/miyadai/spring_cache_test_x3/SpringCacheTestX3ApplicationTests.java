e42b2372a5f95eff67e8af4128fdc694f786ea6a
kohei-miyadai07 <wm25gb87@gmail.com>
2025-01-03 09:46:23 +0900

    feat: CacheResolverとCacheのKeyGenerater勉強用のプロジェクト作成
    refs #feature-001

---
+++ spring-cache-test-x3/src/test/java/com/miyadai/spring_cache_test_x3/SpringCacheTestX3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheTestX3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-cache-test-x4/src/test/java/com/miyadai/spring_cache_test_x4/SpringCacheTestX4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheTestX4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
