2d5bd64fc94c179da7a0140a9608ebb42e4967e5
kohei-miyadai07 <wm25gb87@gmail.com>
2025-01-04 22:31:30 +0900

    feat: キャッシュ更新削除勉強用プロジェクト作成

---
+++ spring-cache-test-x5/src/main/java/com/miyadai/spring_cache_test_x5/SpringCacheTestX5Application.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestX5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheTestX5Application.class, args);
+	}
+
+}
---
+++ spring-cache-test-x5/src/test/java/com/miyadai/spring_cache_test_x5/SpringCacheTestX5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheTestX5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
