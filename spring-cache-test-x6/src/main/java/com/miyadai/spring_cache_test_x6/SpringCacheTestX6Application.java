ab39b8d29abcd22e1bba251e96fe3c47c99e05dc
kohei-miyadai07 <wm25gb87@gmail.com>
2025-01-08 22:24:02 +0900

    feat: キャッシュ更新用のプロジェクト作成

---
+++ spring-cache-test-x6/src/main/java/com/miyadai/spring_cache_test_x6/SpringCacheTestX6Application.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheTestX6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheTestX6Application.class, args);
+	}
+
+}
---
+++ spring-cache-test-x6/src/test/java/com/miyadai/spring_cache_test_x6/SpringCacheTestX6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miyadai.spring_cache_test_x6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheTestX6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
